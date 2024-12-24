ddd4ad6c4288f8f82418e1383d9d823b9cdbb795
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-24 12:20:06 +0300

    Update Album.java

--- Album.java
+++ Album.java
@@ -4,59 +4,48 @@ import java.util.*;
 
 public class Album {
 
-	private String name;
-	private String artist;
-	private ArrayList<Song> songs;
+    private String name;
+    private String artist;
+    private List<Song> songs; 
 	
-	public Album(String name, String artist) {
-		this.name = name;
-		this.artist = artist;
-		this.songs = new ArrayList<Song>();
-	}
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
 	
-	public Album() {
-		
-	}
-	
-	public Song findSong(String title) {
-		
-		for(Song checkedSong : songs) {
-			if(checkedSong.getTitle().equals(title)) {
-				return checkedSong;
-			}
-		}
-		return null;
-	}
-	public boolean addSong(String title, double duration) {
-		if(findSong(title) == null) {
-			songs.add(new Song(title,duration));
-//			System.out.println(title + " successfully added to the List");
-			return true;
-		}
-		else {
-			System.out.println("Song with this name" + title + " already exist in the List");
-			return false;
-		}
-	}
-	public boolean addToPlaylist(int trackNumber, LinkedList<Song> PlayList) {
-		int index = trackNumber - 1;
-		if(index > 0 && index <= this.songs.size()) {
-			PlayList.add(this.songs.get(index));
-			return true;
-		}
-//		System.out.println("This album does not have song with that trackNumber" + trackNumber);
-		return false;
-	}
-	public boolean addToPlayList(String title, LinkedList<Song> PlayList) {
-		for(Song checkedSong : this.songs) {
-			if(checkedSong.getTitle().equals(title)) {
-				PlayList.add(checkedSong);
-				return true;
-			}
-		}
-//		System.out.println(title + " there is no such song in the album");
-		return false;
-	}
-	
-	
+    public Song findSong(String title) {
+        return songs.stream()
+                    .filter(song -> song.getTitle().equals(title))
+                    .findFirst()
+                    .orElse(null);
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) != null) {
+            System.out.println("Song with this name " + title + " already exists in the List");
+            return false;
+        }
+        songs.add(new Song(title, duration));
+        return true;
+    }
+
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playlist) {
+        if (trackNumber < 1 || trackNumber > songs.size()) {
+            System.out.println("This album does not have a song with track number " + trackNumber);
+            return false;
+        }
+        playlist.add(songs.get(trackNumber - 1));
+        return true;
+    }
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playlist) {
+        Song song = findSong(title);
+        if (song != null) {
+            playlist.add(song);
+            return true;
+        }
+        System.out.println("Song with title " + title + " not found in the album.");
+        return false;
+    }
 }
