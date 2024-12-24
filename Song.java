9c756d9829de1284caee8f57d7454c8e5bbdceb2
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-24 12:20:52 +0300

    Update Song.java

--- Song.java
+++ Song.java
@@ -2,29 +2,27 @@ package songPlaylistApplication;
 
 public class Song {
 
-	String title;
-	double duration;
-	
-	
-	public Song(String title, double duration) {
-		this.title = title;
-		this.duration = duration;
-	}
-	public Song() {
-		
-	}
-	public String getTitle() {
-		return title;
-	}
-	
-	public double getDuration() {
-		return duration;
-	}
-	
-	public String toString() {
-		return "Song{" +
-	           "title='" + title + '\'' +
-	           ", duration=" + duration +
-	           '}';
-	}
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+               "title='" + title + '\'' +
+               ", duration=" + duration +
+               '}';
+    }
 }
